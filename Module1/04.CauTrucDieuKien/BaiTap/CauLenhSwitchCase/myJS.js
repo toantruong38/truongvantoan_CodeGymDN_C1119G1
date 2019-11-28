4b9b45fe1dc2782eb03f0fa10f4a3125c6d21073
unknown <luongsexy007@gmail.com>
2019-11-28 17:09:02 +0700

    28/11/2019

---
+++ Module1/04.CauTrucDieuKien/BaiTap/CauLenhSwitchCase/myJS.js
@@ -0,0 +1,16 @@
+function Express()
+{
+    let inputMonth=document.getElementById("inputTxtBox");
+    let result ="";
+    switch(inputMonth.value)
+    {
+        case '1': case '3': case '5': case '7': case '8': case '10': case '12':
+            result = 31; break;
+        case '4': case '6': case '9': case '11':
+             result = 30; break;
+        case '2':
+            result = "28 or 29"; break;
+        default: result = "thang ko hop le";
+    }
+    document.getElementById("showResult").innerText = result;
+}
