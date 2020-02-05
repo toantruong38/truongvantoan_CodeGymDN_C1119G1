498ef078517b0297904656f354c74fcea68a90e5
Admin <luongsexy007@gmail.com>
2020-02-05 09:52:18 +0700

    CS_DB edit

--- Module2/CaseStudy_v2_DB/[New]sql_queries.sql
+++ Module2/CaseStudy_v2_DB/[New]sql_queries.sql
@@ -2,6 +2,7 @@
 use `case_study_db_truongVanToan`;
 
 -- task2;
+-- 2.	Hiển thị thông tin của tất cả nhân viên có trong hệ thống có tên bắt đầu là một trong các ký tự “H”, “T” hoặc “K” và có tối đa 15 ký tự.
 SELECT 
     *
 FROM
@@ -12,6 +13,7 @@ WHERE
         AND LENGTH(nv.hoten) <= 15;
         
 -- task 3;
+-- 3.	Hiển thị thông tin của tất cả khách hàng có độ tuổi từ 18 đến 50 tuổi và có địa chỉ ở “Đà Nẵng” hoặc “Quảng Trị”.
 SELECT 
     *
 FROM
@@ -21,6 +23,7 @@ WHERE
         AND (kh.diachi IN ('Da Nang' , 'Quang Tri'));
     
 -- task 4;
+-- 4.	Đếm xem tương ứng với mỗi khách hàng đã từng đặt phòng bao nhiêu lần. Kết quả hiển thị được sắp xếp tăng dần theo số lần đặt phòng của khách hàng. Chỉ đếm những khách hàng nào có Tên loại khách hàng là “VIP”.
 SELECT 
     kh.hoten, COUNT(hd.id_hopdong) AS 'So lan dat phong'
 FROM
@@ -35,6 +38,7 @@ GROUP BY kh.hoten
 ORDER BY 'So lan dat phong';
 
 -- task 5;
+-- 5.	Hiển thị IDKhachHang, HoTen, TenLoaiKhach, IDHopDong, TenDichVu, NgayLamHopDong, NgayKetThuc, TongTien (Với TongTien được tính theo công thức như sau: ChiPhiThue + SoLuong*Gia, với SoLuong và Giá là từ bảng DichVuDiKem) cho tất cả các Khách hàng đã từng đặt phỏng. (Những Khách hàng nào chưa từng đặt phòng cũng phải hiển thị ra).
 SELECT 
     kh.id_khachhang,
     kh.hoten,
@@ -58,6 +62,7 @@ FROM
     `dich_vu_di_kem` dvdk USING (id_dichvudikem);
     
 -- task 6;
+-- 6.	Hiển thị IDDichVu, TenDichVu, DienTich, ChiPhiThue, TenLoaiDichVu của tất cả các loại Dịch vụ chưa từng được Khách hàng thực hiện đặt từ quý 1 của năm 2019 (Quý 1 là tháng 1, 2, 3).
 SELECT 
     dv.id_dichvu,
     dv.tendichvu,
@@ -99,6 +104,7 @@ WHERE
                 
                 
 -- task 7; >?<
+-- 7.	Hiển thị thông tin IDDichVu, TenDichVu, DienTich, SoNguoiToiDa, ChiPhiThue, TenLoaiDichVu của tất cả các loại dịch vụ đã từng được Khách hàng đặt phòng trong năm 2018 nhưng chưa từng được Khách hàng đặt phòng  trong năm 2019.
 SELECT 
     dv.id_dichvu,
     dv.tendichvu,
@@ -132,7 +138,8 @@ WHERE
             YEAR(hd.ngaylamhopdong) = '2019');
         
 -- task 8
-SELECT DISTINCT
+-- 8.	Hiển thị thông tin HoTenKhachHang có trong hệ thống, với yêu cầu HoThenKhachHang không trùng nhau.
+	SELECT DISTINCT
     kh.hoten
 FROM
     `khach_hang` kh;
@@ -154,6 +161,7 @@ FROM
     
     
 -- task 9; //Khong hieu chi het ??
+-- 9.	Thực hiện thống kê doanh thu theo tháng, nghĩa là tương ứng với mỗi tháng trong năm 2019 thì sẽ có bao nhiêu khách hàng thực hiện đặt phòng.
 SELECT 
     temp.month,
     COUNT(MONTH(hd.ngaylamhopdong)) AS 'So khach hang dang ki',
@@ -172,6 +180,7 @@ GROUP BY temp.month
 ORDER BY temp.month;
 
 -- task 10
+-- 10.	Hiển thị thông tin tương ứng với từng Hợp đồng thì đã sử dụng bao nhiêu Dịch vụ đi kèm. Kết quả hiển thị bao gồm IDHopDong, NgayLamHopDong, NgayKetthuc, TienDatCoc, SoLuongDichVuDiKem (được tính dựa trên việc count các IDHopDongChiTiet).
 SELECT 
     hd.id_hopdong,
     hd.ngaylamhopdong,
@@ -184,6 +193,7 @@ FROM
     `hop_dong_chi_tiet` hdct USING (id_hopdong);
     
 -- task 11
+-- 11.	Hiển thị thông tin các Dịch vụ đi kèm đã được sử dụng bởi những Khách hàng có TenLoaiKhachHang là “VIP” và có địa chỉ là “Vinh” hoặc “Quảng Ngãi”.
 SELECT 
     dvdk.*
 FROM
@@ -202,6 +212,7 @@ WHERE
         OR kh.diachi = 'Quang Ngai');
     
 -- task 12
+-- 12.	Hiển thị thông tin IDHopDong, TenNhanVien, TenKhachHang, SoDienThoaiKhachHang, TenDichVu, SoLuongDichVuDikem (được tính dựa trên tổng Hợp đồng chi tiết), TienDatCoc của tất cả các dịch vụ đã từng được khách hàng đặt vào 3 tháng cuối năm 2019 nhưng chưa từng được khách hàng đặt vào 6 tháng đầu năm 2019.
 SELECT 
     hd.id_hopdong,
     nv.hoten AS 'Ho ten nhan vien',
@@ -235,6 +246,7 @@ WHERE
             'me' NOT BETWEEN '2019-10-01' AND '2019-12-31');
 
 -- task 13
+-- 13.	Hiển thị thông tin các Dịch vụ đi kèm được sử dụng nhiều nhất bởi các Khách hàng đã đặt phòng. (Lưu ý là có thể có nhiều dịch vụ có số lần sử dụng nhiều như nhau).
 SELECT 
     dvdk.*, COUNT(hdct.id_dichvudikem) AS 'So lan su dung'
 FROM
@@ -250,6 +262,7 @@ ORDER BY 'So lan su dung' DESC
 LIMIT 0 , 1;
 
 -- task 14
+-- 14.	Hiển thị thông tin tất cả các Dịch vụ đi kèm chỉ mới được sử dụng một lần duy nhất. Thông tin hiển thị bao gồm IDHopDong, TenLoaiDichVu, TenDichVuDiKem, SoLanSuDung.
 SELECT 
     hd.id_hopdong,
     ldv.tenloaidichvu,
@@ -269,6 +282,7 @@ GROUP BY hdct.id_dichvudikem
 HAVING `So lan su dung` = 1;
 
 -- task 15
+-- 15.	Hiển thi thông tin của tất cả nhân viên bao gồm IDNhanVien, HoTen, TrinhDo, TenBoPhan, SoDienThoai, DiaChi mới chỉ lập được tối đa 3 hợp đồng từ năm 2018 đến 2019.
 SELECT 
     nv.id_nhanvien,
     nv.hoten,
@@ -292,6 +306,7 @@ GROUP BY hd.id_nhanvien
 HAVING `So lan` <= 3;
 
 -- task 16
+-- 16.	Xóa những Nhân viên chưa từng lập được hợp đồng nào từ năm 2017 đến năm 2019.
 delete
 from `nhan_vien` nv
 where not exists(
@@ -301,6 +316,7 @@ inner join `hop_dong` hd using(id_nhanvien)
 where year(hd.ngaylamhopdong) in ('2017','2018','2019'));
 
 -- task 17
+-- 17.	Cập nhật thông tin những khách hàng có TenLoaiKhachHang từ VIP1 lên VIP, chỉ cập nhật những khách hàng đã từng đặt phòng với tổng Tiền thanh toán trong năm 2019 là lớn hơn 10.000.000 VNĐ.
 UPDATE `loai_khach` lk 
 SET 
     lk.tenloaikhach = 'Vip1'
@@ -317,6 +333,7 @@ WHERE
         HAVING `tt` > 10000000);
 
 -- task 18
+-- 18.	Xóa những khách hàng có hợp đồng trước năm 2016 (chú ý rang buộc giữa các bảng).
 delete 
 from `khach_hang` kh
 where exists(
@@ -327,6 +344,7 @@ where year(hd.ngaylamhopdong) <2016);
 
 
 -- task 19
+-- 19.	Cập nhật giá cho các Dịch vụ đi kèm được sử dụng trên 10 lần trong năm 2019 lên gấp đôi.
 UPDATE `dich_vu_di_kem` dvdk 
 SET 
     dvdk.gia = dvdk.gia * 2
@@ -348,6 +366,7 @@ WHERE
 
 
 -- task 20
+-- 20.	Hiển thị thông tin của tất cả các Nhân viên và Khách hàng có trong hệ thống, thông tin hiển thị bao gồm ID (IDNhanVien, IDKhachHang), HoTen, Email, SoDienThoai, NgaySinh, DiaChi.
 SELECT 
     'Nhan vien' AS `Prefix`,
     nv.id_nhanvien,
