f4093a7648b0d603765ab6e304e2dfa26a3bb9ed
Admin <luongsexy007@gmail.com>
2020-02-10 09:03:19 +0700

    m

--- Module2/13.WebAppOverview/[TH]LoginForm/src/LoginServlet.java
+++ Module2/13.WebAppOverview/[TH]LoginForm/src/LoginServlet.java
@@ -6,19 +6,26 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(name = "ServerTimeServlet" ,urlPatterns = "/index")
-public class ServerTimeServlet extends HttpServlet
+@WebServlet(name = "LoginServlet",urlPatterns = "/login")
+public class LoginServlet extends HttpServlet
 {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
     {
+        String us=request.getParameter("username");
+        String ps=request.getParameter("password");
 
+        PrintWriter writer=response.getWriter();
+        writer.println("<html>");
+
+        if(us.equals("admin")&&ps.equals("admin"))
+        {writer.println("<h1>Welcome</h1>");}
+        else writer.println("<h1>Error</h1>");
+
+        writer.println("</html>");
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
     {
-        PrintWriter writer=response.getWriter();
-        writer.println("<html>");
-        writer.println("<h1>Hello world</h1>");
-        writer.println("</html>");
+
     }
 }
---
+++ Module2/13.WebAppOverview/[TH]LoginForm/web/index.jsp
@@ -0,0 +1,23 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Admin
+  Date: 02/10/2020
+  Time: 8:51 AM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>Login WebApp</title>
+  </head>
+  <body>
+  <form action="/login" method="post">
+    <div>
+      <h2>Login</h2>
+      <input name="username" type="text" size="30" placeholder="username"/><br/>
+      <input name="password" type="text" size="30" placeholder="password"/><br/>
+      <button >Submit</button>
+    </div>
+  </form>
+  </body>
+</html>
